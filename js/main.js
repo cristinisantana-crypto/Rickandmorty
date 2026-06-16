12c85c65475e4e998da4017bf87fe5dff553ac76
cristinisantana-crypto <cristini.santana@escola.pr.gov.br>
2026-06-16 10:13:42 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+import { buscarPersonagens } from "./api.js";
+import { renderizarPersonagens, mostrarMensagem } from "./util.js";
+
+const form = document.querySelector('#search-form');
+const input = document.querySelector('#search-input');
+
+async function carregarPersonagens(nome = '') {
+    try {
+        mostrarMensagem('Carregar personagens...');
+    const data = await buscarPersonagens(nome);
+    renderizarPersonagens(data.results);
+    } catch (error) {
+        mostrarMensagem('Nenhum personagem encontrado.');
+    }
+};
+form.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    const nomeDigitado = input.value;
+    await carregarPersonagens(nomeDigitado);
+});
+
+carregarPersonagens();
+
+async function iniciarApp() {
+    const personagens = await buscarPersonagens();
+    renderizarPersonagens(personagens.results);
+
+}
+
+iniciarApp();
